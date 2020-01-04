a2cdb992d3db5ace25963dfa1b8f421578095092
b54690 <belafonte@hotmail.co.uk>
2020-01-04 15:31:25 +0000

    Edit server.js

--- src/app/server.js
+++ src/app/server.js
@@ -5,7 +5,7 @@ const app = express();
 
 // Serve only the static files form the dist directory
 // app.use(express.static(__dirname,  '/dist/lush-angular'));
-app.use(express.static(__dirname,  null));
+app.use(express.static(__dirname,  '/dist/lush-angular'));
 
 app.get('*', function(req,res) {
     res.sendFile(path.join(__dirname, '/dist/lush-angular/index.html'));
@@ -13,4 +13,5 @@ app.get('*', function(req,res) {
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || port);
+
 
